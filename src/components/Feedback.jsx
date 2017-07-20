4f9ee3c31ce1924d4d3f9b9d5431c03c611fcd09
Amin Al Hazwani <amin.alhazwani@gmail.com>
2017-07-20 15:30:56 +0200

    [style] emoji instead of text in share feedback

--- src/components/Feedback.jsx
+++ src/components/Feedback.jsx
@@ -21,17 +21,19 @@ const Feedback = React.createClass({
     /* eslint-disable react/jsx-no-literals */
     return(
       <div className="center mw7 ph3 mb4 mb5-l">
-        <div className="w-100 ba b--light-gray br2 pa3 flex-ns justify-between-ns items-center-ns">
-          <p className="mt0 mb3 mb0-ns lh-copy pt1-ns">Tell us, was the content of this page helpful?</p>
+        <div className="w-100 ba b--light-gray br2 ph3 pb1 flex-ns justify-between-ns items-center-ns">
+          <p className="mt0 mb3 mb0-ns lh-copy pt2-ns">Tell us, was the content of this page helpful?</p>
           <p className="ma0">
-            <a className="mr3 no-underline bg-green white font-smoothing br-100 dib tc w2 h2 pt1 lh-copy fw6"
+            <a className="mr3 no-underline f3 lh-copy dib pt2"
                 href="#!"
                 id="thumbs-up"
-            >+</a>
-            <a className="no-underline bg-red white font-smoothing br-100 dib tc w2 h2 pt1 lh-copy fw6"
+            >🙂
+            </a>
+            <a className="no-underline f3 lh-copy dib pt2"
                 href="https://github.com/FirefoxUX/photon/issues/new"
                 id="thumbs-down"
-            >–</a>
+            >😟
+            </a>
           </p>
         </div>
       </div>)
